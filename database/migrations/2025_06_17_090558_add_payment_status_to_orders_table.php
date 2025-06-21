16bb42c3af95946d0b5ba24aa07bacf95dd1ae91
ssentongoJoshua <ssentongoj05hu4@gmail.com>
2025-06-21 12:46:05 +0300

    Add payment_status column to orders table

--- database/migrations/2025_06_17_090558_add_payment_status_to_orders_table.php
+++ database/migrations/2025_06_17_090558_add_payment_status_to_orders_table.php
@@ -11,11 +11,9 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('orders', function (Blueprint $table) {
-            $table->engine = 'InnoDB';
-            $table->id();
-            $table->timestamps();
-        });
+      Schema::table('orders', function (Blueprint $table) {
+        $table->string('payment_status')->default('pending')->after('id');
+      });
     }
 
     /**
