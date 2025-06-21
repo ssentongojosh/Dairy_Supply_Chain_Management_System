eb5885d70a5a895b3a73ca95c2b25c7b430416f1
victory-pixel <victoryojok2004@gmail.com>
2025-06-21 13:02:55 +0300

    Your message about what you changed

--- database/migrations/2025_06_15_000001_create_messages_table.php
+++ database/migrations/2025_06_15_000001_create_messages_table.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void
     {
+     
         Schema::create('messages', function (Blueprint $table) {
             $table->id();
             $table->foreignId('sender_id')->constrained('users')->onDelete('cascade');
