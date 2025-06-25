60386226c1f1e569d0245ccc6be9727797ef95b7
ssentongoJoshua <ssentongoj05hu4@gmail.com>
2025-06-25 22:07:52 +0300

    set up the reporting module

---
+++ app/Http/Controllers/ReportController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ReportController extends Controller
+{
+    //
+    public function index()
+    {
+        // This method can be used to display a list of reports or a dashboard
+        return view('reports.settings');
+    }
+}
